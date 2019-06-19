3b16cfbd88e74347ead29bf1810512097b701341
谢赤心 <xiechixin@ilongyuan.com.cn>
2019-06-19 21:07:47 +0800

    sql优化

--- Application/Common/Conf/website.php
+++ Application/Common/Conf/website.php
@@ -1,6 +1,6 @@
 <?php 
 		return array(
-			'WEB_TITLE' => '充值',
+			'WEB_TITLE' => '话充平台',
 			'DOMAIN' => 'recharge.com',
 			'MODULE_ALLOW_LIST'   => array('Home','User','Admin','Install', 'Weixin','Pay','Cashier','Agent','Payment'),
 			'URL_MODULE_MAP'  => array('admin'=>'admin', 'agent'=>'user', 'user'=>'user'),
