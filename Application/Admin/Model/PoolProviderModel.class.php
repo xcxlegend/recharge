2943743c63a5fd38fdf272999af9106a72c52f2a
being <871299266@qq.com>
2019-06-22 18:52:11 +0800

    号码商模块
    Signed-off-by: being <871299266@qq.com>

---
+++ Application/Admin/Model/PoolProviderModel.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin\Model;
+
+class PoolProviderModel extends BaseModel
+{
+
+    
+
+    public function getList($where)
+    {
+
+        $count = $this->where($where)->count();
+        $page = new \Think\Page($count, parent::PAGE_LIMIT);
+        $list = $this->where($where)->limit($page->firstRow, $page->listRows)->order('id DESC')->select();
+        return array(
+            'list' => $list,
+            'page' => $page->show(),
+        );
+    }
+
+}
