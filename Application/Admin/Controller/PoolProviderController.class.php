a81d13a6cab7088f47890d5be437307faf5590af
being <871299266@qq.com>
2019-07-20 15:30:56 +0800

    bug

--- Application/Admin/Controller/PoolProviderController.class.php
+++ Application/Admin/Controller/PoolProviderController.class.php
@@ -329,7 +329,7 @@ class PoolProviderController extends BaseController
         if(!empty($param['pid'])){
             $maps['pid'] = $param['uid'];
         }
-        $maps['money'] = array('in','1,2,3');
+        $maps['type'] = array('in','1,2,3');
         
         $count          = M('PoolMoneychange')->where($maps)->count();
 
