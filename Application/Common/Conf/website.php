f604fd69b61bc912278b141e335e09a2a89712be
wakedreaming <871299266@qq.com>
2019-10-27 22:20:11 +0800

    bug

--- Application/Common/Conf/website.php
+++ Application/Common/Conf/website.php
@@ -3,7 +3,7 @@
 			'WEB_TITLE' => '话充平台',
 			'DOMAIN' => 'recharge.in:8001',
 			'MODULE_ALLOW_LIST'   => array('Home','User','Admin','Install', 'Weixin','Pay','Cashier','Agent','Payment','Pool'),
-			'URL_MODULE_MAP'  => array('adminpay'=>'admin', 'agent'=>'user', 'user'=>'user', 'pool'=>'pool'),
+			'URL_MODULE_MAP'  => array('admin'=>'admin', 'agent'=>'user', 'user'=>'user', 'pool'=>'pool'),
 			'LOGINNAME' => 'Login',
 			'HOUTAINAME' => 'admin',
 		);
