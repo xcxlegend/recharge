ba752507bbec752fe48136dd3a5f631876cb7022
wakedreaming <871299266@qq.com>
2019-10-28 23:05:37 +0800

    demoAPI

--- demo/index.php
+++ demo/index.php
@@ -151,11 +151,15 @@ $product_name="测试订单";
         <li>
             <span>商品名称：</span>
             <span><?php echo $product_name;?></span>
+
+            <span style="margin-left:30px">密钥：</span>
+        <strong><input type="text" name="apikey" style="border:solid 1px #ddd;height:30px;width:260px"></strong>
+            
         </li>
         <li>
             <span>订单编号：</span>
             <span><?php echo $pay_orderid;?></span>
-        </li>
+            </li>
     </ul>
     <div class="order-amount12-right">
         <span>订单金额：</span>
