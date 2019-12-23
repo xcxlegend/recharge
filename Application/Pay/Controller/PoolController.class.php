8493d859090e7602d89b26185d73f1a107f06564
wakedreaming <871299266@qq.com>
2019-12-24 00:36:39 +0800

    n

--- Application/Pay/Controller/PoolController.class.php
+++ Application/Pay/Controller/PoolController.class.php
@@ -84,8 +84,8 @@ class PoolController extends PayController
         $isTrans = false;
 
 
-        if ($provider_config['limit_num'] >= 0 && ($phone_num > $provider_config['limit_num'] || $phone_num==0)) {
-            if($provider_config['transe']==0){
+        if ($provider_config['limit_num'] >= 0) {
+            if($provider_config['transe']==0 && $phone_num > $provider_config['limit_num'] && $provider_config['limit_num']!=0){
                 $this->result_error("号码超出库存", true);
             }else{
                 $isTrans = true;
